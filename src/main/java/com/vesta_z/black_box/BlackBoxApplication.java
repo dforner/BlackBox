340155865e930006ee7f16cbf13795627f5d63ea
Daniel Forner <daniel.forner@visual-limes.com>
2022-12-11 11:25:18 +0100

    Initial commit

---
+++ src/main/java/com/vesta_z/black_box/BlackBoxApplication.java
@@ -0,0 +1,14 @@
+package com.vesta_z.black_box;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vesta_z/black_box/BlackBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vesta_z.black_box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
